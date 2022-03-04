90853d9cf996fc5061ba6ec8769d85ed6082a6ed
DongKyoun <gkehdrbs36@naver.com>
2022-03-04 23:50:44 +0900

    conversion

---
+++ importantConcept/charToCode.java
@@ -0,0 +1,14 @@
+package importantConcept;
+
+// 형변환
+
+public class charToCode {
+  public static void main(String[] args) {
+    char ch = 'A';
+    int code = (int)ch;
+
+    //'A'의 유니코드 출력
+    System.out.println(code);
+   
+  }
+}
