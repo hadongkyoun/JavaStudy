2670c102b09fa79f742fbfc5e8b7af7d31136d61
DongKyoun <gkehdrbs36@naver.com>
2022-03-12 14:26:25 +0900

    Factorial Test 2

--- TestFolder/FactoralTest.java
+++ TestFolder/FactoralTest.java
@@ -1,15 +1,25 @@
 package TestFolder;
 
 public class FactoralTest {
-  public static void main(String[] args){
-    int result = factorial(4);
-    System.out.println(result);
-  }
-  static int factorial(int n){
-    if(n==1)
-      return 1;
-    else
+  static long factorial(int n){
+    if(n<0 || n>20) return -1;
+    if(n==1) return 1;
       return n * factorial(n-1);
+  }
+  
+  
+  public static void main(String[] args){
+    long result = 0;
+    int n = 21;
+
+    for(int i=1; i<=n; i++){
+      result = factorial(i);
+      
+      if(result==-1)
+        System.out.println("유효하지않은 값");
+      else
+        System.out.printf("%2d!=%20d\n", i, result);
+    }
   }
 }
 
