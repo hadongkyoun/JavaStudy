a9507f45d5e0064d0b6585143499154aef4cc5f1
DongKyoun <gkehdrbs36@naver.com>
2022-03-14 15:48:00 +0900

    Using parameter Constructor

--- TestFolder/CarTest.java
+++ TestFolder/CarTest.java
@@ -5,23 +5,25 @@ class Car{
   String gearType;
   int door;
 
-  Car(){} // 기본 생성자 써주기 ( 오류 방지 )
+  Car(){
+    this("white", "auto", 4);
+  } 
+
+  Car(String color){
+    this(color, "auto", 4);
+  }
 
   Car(String c, String g, int d){
-    color = c;
-    gearType = g;
-    door = d;
+    this.color = c;
+    this.gearType = g;
+    this.door = d;
   }
 }
 
 class CarTest {
   public static void main(String[] args){
     Car c1 = new Car();
-    c1.color = "WHITE";
-    c1.gearType = "auto";
-    c1.door = 4;
-
-    Car c2 = new Car("BLACK", "manual", 2);
+    Car c2 = new Car("BLUE");
     
     System.out.println("Car color = "+ c1.color + ", GearType = "+ c1.gearType + ", door num = " + c1.door);
     System.out.println("Car color = "+ c2.color + ", GearType = "+ c2.gearType + ", door num = " + c2.door);
