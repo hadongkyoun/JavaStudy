42ac65261e97d5d192bf504c6a616099a252697c
DongKyoun <gkehdrbs36@naver.com>
2022-03-14 23:28:50 +0900

    Block Test

---
+++ TestFolder/BlockTest.java
@@ -0,0 +1,24 @@
+package TestFolder;
+
+class BlockTest{
+  static {
+    //클래스 초기화 블럭
+    System.out.println("static { }");
+  }
+  {
+    //인스턴스 초기화 블럭
+    System.out.println("{ }");
+  }
+  
+  BlockTest() {
+    System.out.println("생성자");
+  }
+  
+  public static void main(String[] args){
+    System.out.println("BlockTest bt = new BlockTest( );");
+    BlockTest bt = new BlockTest();
+
+    System.out.println("BlockTest bt2 = new BlockTest( );");
+    BlockTest bt2 = new BlockTest();
+  }
+}
