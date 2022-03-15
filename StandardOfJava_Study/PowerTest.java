767bba12d9075c8c767aee5bc0e4e2182d6f38de
DongKyoun <gkehdrbs36@naver.com>
2022-03-15 22:35:14 +0900

    Standard of java study ( important concept )

---
+++ StandardOfJava_Study/PowerTest.java
@@ -0,0 +1,18 @@
+package StandardOfJava_Study;
+
+public class PowerTest {
+  public static void main(String[] args){
+    int x = 2;
+    int n = 5;
+    long result = 0;
+
+    for(int i=1; i<=n; i++){
+      result += power(x, i);
+    }
+    System.out.println(result);
+  }
+  static long power(int x, int i){
+    if(i==1) return x;
+      return x * power(x, i-1);
+  }
+}
