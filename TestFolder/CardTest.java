44f855b6b294278d92f17fff3997b05c21fc2bcc
DongKyoun <gkehdrbs36@naver.com>
2022-03-15 21:05:33 +0900

    change 'Card' class name

--- TestFolder/CardTest.java
+++ TestFolder/CardTest.java
@@ -1,6 +1,6 @@
 package TestFolder;
 
-class Card {
+class Card2 {
   //인스턴스 변수
   String kind;
   int number;    
@@ -11,26 +11,26 @@ class Card {
 
 class CardTest{
   public static void main(String[] args) {
-    System.out.println("카드 폭 =" + Card.width);
-    System.out.println("카드 높이 =" + Card.height);
+    System.out.println("카드 폭 =" + Card2.width);
+    System.out.println("카드 높이 =" + Card2.height);
 
     //카드 ♥ 7
-    Card c1 = new Card();
+    Card2 c1 = new Card2();
     c1.kind = "Heart";
     c1.number = 7;
     
     //카드 ♠ 4
-    Card c2 = new Card();
+    Card2 c2 = new Card2();
     c2.kind = "Space";
     c2.number = 4;
 
-    System.out.println("c1 은 "+ c1.kind +", "+ c1.number +"이며, 크기는 "+Card.width+", "+Card.height+"이다.");
-    System.out.println("c1 은 "+ c2.kind +", "+ c2.number +"이며, 크기는 "+Card.width+", "+Card.height+"이다.");
+    System.out.println("c1 은 "+ c1.kind +", "+ c1.number +"이며, 크기는 "+Card2.width+", "+Card2.height+"이다.");
+    System.out.println("c1 은 "+ c2.kind +", "+ c2.number +"이며, 크기는 "+Card2.width+", "+Card2.height+"이다.");
 
-    Card.width = 50;
-    Card.height = 80;
+    Card2.width = 50;
+    Card2.height = 80;
 
-    System.out.println("c1 은 "+ c1.kind +", "+ c1.number +"이며, 크기는 "+Card.width+", "+Card.height+"이다.");
-    System.out.println("c1 은 "+ c2.kind +", "+ c2.number +"이며, 크기는 "+Card.width+", "+Card.height+"이다.");
+    System.out.println("c1 은 "+ c1.kind +", "+ c1.number +"이며, 크기는 "+Card2.width+", "+Card2.height+"이다.");
+    System.out.println("c1 은 "+ c2.kind +", "+ c2.number +"이며, 크기는 "+Card2.width+", "+Card2.height+"이다.");
   }
 }
