9a84bc0c35ab31a2cc5e42a5d8f2a1b689e6937a
DongKyoun <gkehdrbs36@naver.com>
2022-03-16 14:36:08 +0900

    fix error

--- StandardOfJava_Study/PointTest.java
+++ StandardOfJava_Study/PointTest.java
@@ -2,8 +2,10 @@ package StandardOfJava_Study;
 
 public class PointTest {
   public static void main(String[] args){
-
-
+      Point3D p3 = new Point3D();
+      System.out.println("p3.x="+ p3.x);
+      System.out.println("p3.y="+ p3.y);
+      System.out.println("p3.z="+ p3.z);
   }
 }
 
