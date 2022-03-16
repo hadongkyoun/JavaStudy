05d1c93c30e15b89f15afeb061da29d7534b9d26
DongKyoun <gkehdrbs36@naver.com>
2022-03-16 13:30:46 +0900

    Super test

---
+++ StandardOfJava_Study/SuperTest.java
@@ -0,0 +1,21 @@
+package StandardOfJava_Study;
+
+public class SuperTest {
+  public static void main(String[] args){
+    
+  }
+}
+
+class Parent{
+  int x = 10;
+}
+
+class Child extends Parent{
+  void method(){
+    System.out.println("x=" + x);
+
+    // this 와 super은 근본적으로 같으나, 구별의 차이
+    System.out.println("this.x=" + this.x);
+    System.out.println("super.x=" + super.x); //x는 부모(Parent Class)의 멤버변수
+  }
+}
