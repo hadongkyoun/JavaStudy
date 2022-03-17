3ee30548046ebf29c6013ef7dc9bd6a4f7def8ff
DongKyoun <gkehdrbs36@naver.com>
2022-03-17 17:54:42 +0900

    same class is exist on the same package

--- ObjectOrientedProgramming/ProductTest.java
+++ ObjectOrientedProgramming/ProductTest.java
@@ -1,5 +1,5 @@
 package ObjectOrientedProgramming;
-class Product{
+class Product2{
   static int count = 0; //생성된 인스턴스의 수를 저장하기 위한 변수
   int serialNo;         //인스턴스 고유의 번호
 
@@ -12,14 +12,14 @@ class Product{
 
 public class ProductTest {
   public static void main(String[] args){
-    Product p1 = new Product();
-    Product p2 = new Product();
-    Product p3 = new Product();
+    Product2 p1 = new Product2();
+    Product2 p2 = new Product2();
+    Product2 p3 = new Product2();
 
     System.out.println("p1의 제품번호: " + p1.serialNo);
     System.out.println("p1의 제품번호: " + p2.serialNo);
     System.out.println("p1의 제품번호: " + p3.serialNo);
     
-    System.out.println("생성된 제품의 수: " + Product.count);
+    System.out.println("생성된 제품의 수: " + Product2.count);
   }
 }
